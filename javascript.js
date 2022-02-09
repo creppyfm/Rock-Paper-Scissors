4a594d08b33ea92b4319139a3b405243e3368549
Foli Creppy <creppyfm@gmail.com>
2022-02-09 01:25:54 -0500

    built computerPlay function

---
+++ javascript.js
@@ -0,0 +1,59 @@
+console.log("Hello World!")
+
+
+// Planning / Pseudocode
+// 
+// What's the problem? 
+//    Need to create a game that ACCEPTS USER INPUT OF ONE OF THREE OPTIONS, EACH OPTION ACCEPTED IN ALL CAPS, LOWERCASE, OR A MIXTURE OF BOTH, 
+//    COMPARES THE VALUE OF THE USERS INPUT WITH A RANDOMLY GENERATED COMPUTER VALUE, DETERMINES WHETHER THE USER OR THE COMPUTER HAVE THE 
+//    HIGHER VALUE (WIN,) OR IF THEIR VALUES ARE EQUAL (TIE). The gamme must also PRINT RELEVANT STRINGS TO ANNOUNCE WINNER/LOSER/TIE.
+//    
+//  Pseudocode        
+//
+//
+// let response;
+
+
+//    function beginGame(let response = prompt("Do you want to play?")) {
+
+//    };
+//-----------------------------------------------------------------------------------------------------------------//
+ // Rock, Paper, Scissors
+//-----------------------------------------------------------------------------------------------------------------//
+
+
+
+
+// creating computer's play
+
+   function computerPlay() {
+        let randNumber = Math.floor(Math.random() * (2 - 0 + 1) + 0);             // picks a random number between 0 and 2 (inclusive) - (max - min + 1) + min
+        let compChoice = ['Rock', 'Paper', 'Scissors'];                                 // creates array that includes our three choices  
+        return compChoice[randNumber];                            // assigns new value to 'compChoice' using the 'randNumber' function we defined above 
+        };                                                          //^^ returns the random selection from the array
+
+ console.log(computerPlay());                                                    // prints the function 'computerPlay()' in console
+
+// alert(computerPlay());                                                        // prints the function 'computerPlay()' in browser prompt
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
