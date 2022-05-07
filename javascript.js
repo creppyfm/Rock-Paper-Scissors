3facd1476cadd7228c2d5858f96d033dc0a2fa81
Foli Creppy <97473588+creppyfm@users.noreply.github.com>
2022-05-07 16:17:22 -0400

    Update javascript.js
    Deleted more unnecessary comments

--- javascript.js
+++ javascript.js
@@ -17,7 +17,7 @@ function thaGame_R_P_S_() {
     
 
 
-    for (let a = 0; a < 100000; a++) {                                                                                                                // save this for later maybe?? (let play = prompt("Would you like to play?"); play == 'YES' || play == 'Y'; play++)
+    for (let a = 0; a < 100000; a++) {                                                                                                               
         
         if (play == 'YES' || play == 'Y') {  
             console.log("Fantastic! esssketit.\n" + game())
@@ -26,11 +26,11 @@ function thaGame_R_P_S_() {
     // creating computer's play
 
     function computerPlay() {
-            let compChoice = ['Rock', 'Paper', 'Scissors'];                                                                                              // creates array that includes our three choices  
-            let randNumber = Math.floor(Math.random() * (2 - 0 + 1) + 0);                                                                                 // picks a random number between 0 and 2 (inclusive) - (max - min + 1) + min
-            let compSelection = compChoice[randNumber];                                                                                                    // creates 'compSelection' using the 'randNumber' function we defined above 
-            let computerSelection = compSelection.toUpperCase();                                                                                     // converts compSelection to Uppercase for easier comparison to user input
-            return computerSelection;                                                                                                                           //^^ returns the random, capitalized selection from the array 
+            let compChoice = ['Rock', 'Paper', 'Scissors']; 
+            let randNumber = Math.floor(Math.random() * (2 - 0 + 1) + 0);   
+            let compSelection = compChoice[randNumber];   
+            let computerSelection = compSelection.toUpperCase(); 
+            return computerSelection;  
 
             };                                                                            
 
@@ -39,9 +39,9 @@ function thaGame_R_P_S_() {
     // creating playerPlay function
 
         function playerPlay() {                                                 
-            let choice = prompt("What's your move?");                                                                                                    // create 'choice' var to accept user input
-            let playerSelection = choice.toUpperCase();                                                                                                // copy capitalized 'choice' to playerSelection for easier comparison to cpu input 
-            return playerSelection;                                                                                                                      // returns capitalized player input
+            let choice = prompt("What's your move?");  
+            let playerSelection = choice.toUpperCase();   
+            return playerSelection; 
         }
 
 
@@ -51,43 +51,43 @@ function thaGame_R_P_S_() {
 
 
 
-        function playRound(playerPlay, computerPlay) {                                                                                                        // create 'playRound' function
+        function playRound(playerPlay, computerPlay) {
 
-            if (((playerPlay === 'ROCK') && (computerPlay === 'ROCK')) || ((playerPlay === 'PAPER')                                                                 // checks for ties: compares user/comp functions with all possible results
-                && (computerPlay === 'PAPER')) || ((playerPlay === 'SCISSORS') && (computerPlay === 'SCISSORS'))) {                                               // to determine whether or not condition has been met (pP === cP), triggering the
+            if (((playerPlay === 'ROCK') && (computerPlay === 'ROCK')) || ((playerPlay === 'PAPER')  
+                && (computerPlay === 'PAPER')) || ((playerPlay === 'SCISSORS') && (computerPlay === 'SCISSORS'))) {  
                 let tie = ("It's a tie! " +playerPlay+ " is equal to " +computerPlay+"\n Your Score: " +playScore+ " Computer Score: " +compScore);                   
-                return (tie);                                                                                                                                       // return statement  
+                return (tie);   
 
-                }   else if ((playerPlay === 'ROCK') && (computerPlay === 'PAPER') || ((playerPlay === 'PAPER')                                                     //  grouped all losing situations to trigger lose string
+                }   else if ((playerPlay === 'ROCK') && (computerPlay === 'PAPER') || ((playerPlay === 'PAPER')    
                         && (computerPlay === 'SCISSORS')) || ((playerPlay === 'SCISSORS') && (computerPlay === 'ROCK'))) {  
-                        compScore = ++compScore;                                                                                                                         // adds 1 each time condition is met, starting from 0
+                        compScore = ++compScore;             
                         let cWin = ("You lose " +computerPlay+ " beats " +playerPlay+"\n Your Score: " +playScore+ " Computer Score: " +compScore);
-                            return (cWin);                                                                                                                                     // added score variables
+                            return (cWin); 
 
-                        }   else if ((playerPlay === 'ROCK') && (computerPlay === 'SCISSORS') || ((playerPlay === 'SCISSORS')                                        //  grouped all winning situations to trigger win string
+                        }   else if ((playerPlay === 'ROCK') && (computerPlay === 'SCISSORS') || ((playerPlay === 'SCISSORS')  
                                 && (computerPlay === 'PAPER')) || ((playerPlay === 'PAPER') && (computerPlay === 'ROCK'))) {
-                                    playScore = ++playScore;                                                                                                            // adds 1 each time condition is met, starting from 0
-                                    let pWin = ("You Win! " +playerPlay+ " beats " +computerPlay+"\n Your Score: " +playScore+ " Computer Score: " +compScore);         // created 'pWin', 'cWin', 'tie', and 'invalid' 
+                                    playScore = ++playScore;  
+                                    let pWin = ("You Win! " +playerPlay+ " beats " +computerPlay+"\n Your Score: " +playScore+ " Computer Score: " +compScore); 
                                     return (pWin);                                                                                                                                                                          
                                     
                                 }   else  {
                                     let invalid = ("Please enter a valid choice.");
                                     return (invalid)
                                 }
-        }                                                                                                                                                              // GOT IT!  // Now just have to account for empty strings and null/undefined      
+        }                                                            
 
 
     // creating the 'game' function
 
 
         function game() {
-            for (let i = 0; i < 5; i++) {                                                                                                                              // loops input 5 times
+            for (let i = 0; i < 5; i++) { 
                 console.log(playRound(playerPlay(), computerPlay()));
             }
 
         
-                if (playScore > compScore) {                                                                                                                            // conditions to determine a winner
-                    return ("You have defeated the Computer! Congratulations!\nFinal Score\nYou: " +playScore+ " Computer: " +compScore);                              // returns final determination, and each player's final score
+                if (playScore > compScore) {   
+                    return ("You have defeated the Computer! Congratulations!\nFinal Score\nYou: " +playScore+ " Computer: " +compScore);  
 
                 }   else if (playScore < compScore) {
                     return ("The Computer has defeated you. Practice is in order.\nFinal Score\nYou: " +playScore+ " Computer: " +compScore); 
